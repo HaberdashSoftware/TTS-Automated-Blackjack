7cc374bc352f8baebe44313362fd2dde3d6bc398
my_hat_stinks <n.healy@gmx.com>
2018-11-14 01:18:26 +0000

    Fixed Bonus Round powerup

--- powerup injection/Bonus.lua
+++ powerup injection/Bonus.lua
@@ -1,22 +1,7 @@
 
 function powerupUsed( d ) -- data keys: setTarget zone, powerup object, setUser zone
 	Global.call( "forwardFunction", {function_name="clearBonus", data={}} )
-	-- Global.call( "forwardFunction", {function_name="bonusRound", data={}} )
-	
-	local bonusTable = Global.getTable("bonusTable")
-	local bonusZone = Global.getVar("bonusZone")
-	
-	local chosenBonus = math.random(1, #bonusTable)
-	local params = {}
-	params.position = bonusZone.getPosition()
-	params.position.y = params.position.y - 1.7
-	params.callback = "activateBonus"
-	params.callback_owner = Global
-	bonusObject = bonusTable[chosenBonus].takeObject(params)
-	bonusObject.setColorTint({r=0.25,g=0.25,b=0.25})
-		
-	Global.setVar("chosenBonus", chosenBonus)
-	Global.setVar("bonusObject", bonusObject)
+	Global.call( "forwardFunction", {function_name="addBonus", data={}} )
 	
 	destroyObject(d.powerup)
 	
