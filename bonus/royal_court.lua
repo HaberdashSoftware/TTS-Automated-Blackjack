0d19613cbc69e061d486562baef70da13d4b65a9
my_hat_stinks <n.healy@gmx.com>
2018-11-29 18:12:53 +0000

    Bonus rounds now have an "Expired" state instead of being removed.

--- bonus/royal_court.lua
+++ bonus/royal_court.lua
@@ -9,26 +9,39 @@ function onDeploy()
 	self.setDescription( bonusDesc.."In effect next hand." )
 end
 function onRoundStart()
-	self.setColorTint({r=1,g=1,b=1})
-	
-	IsActive = true
 	RoundsRemaining = RoundsRemaining - 1
 	
 	if RoundsRemaining<0 then
-		self.destruct()
+		Expire()
 		return
+	elseif RoundsRemaining==1 then
+		self.setDescription( ("%s%i hand remaining"):format(bonusDesc, RoundsRemaining) )
 	elseif RoundsRemaining>0 then
 		self.setDescription( ("%s%i hands remaining"):format(bonusDesc, RoundsRemaining) )
 	else
 		self.setDescription( bonusDesc.."Final Hand" )
 	end
+	
+	self.setColorTint({r=1,g=1,b=1})
+	
+	IsActive = true
 end
 function onRoundEnd()
 	if RoundsRemaining==0 then
-		self.destruct()
+		Expire()
 	end
 end
 function isActive() if IsActive then return true end end
+
+function Expire()
+	IsActive = false
+	RoundsRemaining = -1
+	
+	self.setColorTint({r=0.05,g=0.05,b=0.05})
+	
+	self.setDescription( ("Ended"):format(bonusDesc, RoundsRemaining) )
+end
+
 
 local Royals = {["King"]=true, ["Queen"]=true, ["Jack"]=true, ["Ace"]=true}
 -- local Royals = {["Joker"]=true}
@@ -101,9 +114,6 @@ function dealPlayer( data )
 		if newDeck then
 			destroyObject(newDeck)
 		end
-		
-		-- placeCard(pos, true, set, true)
-		-- lastCard = mainDeck.takeObject({position=pos, flip=flipBool, callback="cardPlacedCallback", callback_owner=Global, params={targetPos=pos, flip=flipBool, set=set, isStarter=isStarter}})
 		
 		return true
 	end
--- bonus/rupee_pull.lua
+++ bonus/rupee_pull.lua
@@ -1,10 +1,5 @@
 
 function onDeploy()
-	self.createButton({
-		label="Give", click_function="DoPull", function_owner=self,
-		position={0,0,0}, rotation={0,0,0}, width=450, height=450, font_size=150
-	})
-	
 	self.setColorTint({r=1,g=1,b=1})
 end
 function onRoundStart()
@@ -12,12 +7,6 @@ function onRoundStart()
 	self.destruct()
 end
 
-function DoPull(o, color)
-	if color == "Black" or Player[color].promoted or Player[color].host then
-		Global.call( "forwardFunction", {function_name="deployRupees", data={}} )
-		self.destruct()
-	end
-end
 function GiveRupeePowerup()
 	local seated = getSeatedPlayers()
 	for i=1,#seated do -- Convert to reference table, saves us looping multiple times
