1c396f83f1a3d5b77b9e40b5c70b9c539d76da8e
my_hat_stinks <n.healy@gmx.com>
2018-12-02 00:16:18 +0000

    Minigame and Ruppe bonuses will no longer be cleared after use
    Rupee pull can now spawn when minigame rounds start, instead of only standard rounds
    Altered Rupee pull spawn position so it's no lunger under cards

--- bonus/minigame.lua
+++ bonus/minigame.lua
@@ -1,10 +1,16 @@
 
+Expired = false
 function onDeploy()
 	self.setColorTint({r=1,g=1,b=1})
+	Expired = false
 end
 function preRoundStart()
+	if Expired then return end
+	Expired = true
+	self.setColorTint({r=0.05,g=0.05,b=0.05})
+	self.setDescription("Ended")
+	
 	Global.call( "forwardFunction", {function_name="beginMiniGame", data={}} )
-	self.destruct()
 	
 	return true
 end
--- bonus/rupee_pull.lua
+++ bonus/rupee_pull.lua
@@ -1,12 +1,19 @@
 
+Expired = false
 function onDeploy()
 	self.setColorTint({r=1,g=1,b=1})
+	Expired = false
 end
-function onRoundStart()
+function preRoundStart()
+	if Expired then return end
+	Expired = true
+	self.setColorTint({r=0.05,g=0.05,b=0.05})
+	self.setDescription("Ended")
+	
 	GiveRupeePowerup()
-	self.destruct()
 end
 
+local desc = "To use, drop into your own zone while you are not in play."
 function GiveRupeePowerup()
 	local seated = getSeatedPlayers()
 	for i=1,#seated do -- Convert to reference table, saves us looping multiple times
@@ -17,8 +24,7 @@ function GiveRupeePowerup()
 	local sets = Global.getTable("objectSets")
 	for i=#sets,1,-1 do
 		if seated[sets[i].color] then
-			local pos = sets[i].zone.getPosition()
-			pos.y = pos.y + 2
+			local pos = sets[i].zone.positionToWorld({0.5,-0.4,-0.5})
 			
 			local clone = self.clone({position = pos})
 			clone.setPosition(pos)
@@ -27,7 +33,7 @@ function GiveRupeePowerup()
 			clone.setLuaScript("")
 			
 			clone.setName( "Random rupee pull" )
-			clone.setDescription( Player[sets[i].color].steam_id .." - ".. Player[sets[i].color].steam_name )
+			clone.setDescription( ("%s - %s\n\n%s"):format(Player[sets[i].color].steam_id, Player[sets[i].color].steam_name, desc) )
 		end
 	end
 end
